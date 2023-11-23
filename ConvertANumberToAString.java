ad34bfe8c92183b9ee3cd46ce5f81332c4cf4bdb
Ann-Po <132414247+Ann-Po@users.noreply.github.com>
2023-11-22 22:39:19 -0500

    Add 4 solved kata

---
+++ ConvertANumberToAString.java
@@ -0,0 +1,21 @@
+package CodewarsJava;
+
+/*
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+ */
+
+public class ConvertANumberToAString {
+
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberToString(999));
+    }
+}
---
+++ YouCantCodeUnderPressure1.java
@@ -0,0 +1,16 @@
+package CodewarsJava;
+
+
+/*
+Code as fast as you can! You need to double the integer and return it.
+ */
+
+public class YouCantCodeUnderPressure1 {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+
+    public static void main (String[] args) {
+        System.out.println(doubleInteger(5));
+    }
+}
